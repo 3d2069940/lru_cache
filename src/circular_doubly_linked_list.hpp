a2d41f05edd1fc41ec72e2aaa0c759ac3062611c
3d2069940 <complete.toni@mail.ru>
2025-02-07 07:34:04 -0500

    circular doubly-linked list and tests are now done

--- src/circular_doubly_linked_list.hpp
+++ src/circular_doubly_linked_list.hpp
@@ -42,36 +42,189 @@ public:
 //----------------------------------------------------------------------------
   using NodeT = CircularDoublyLinkedListNode<ValueT>;
 //----------------------------------------------------------------------------
-  CircularDoublyLinkedList() = delete;
-  CircularDoublyLinkedList(const CircularDoublyLinkedList &) = delete;
-  CircularDoublyLinkedList(CircularDoublyLinkedList &&) = delete;
-  CircularDoublyLinkedList & operator=(const CircularDoublyLinkedList &) = delete;
-  CircularDoublyLinkedList & operator=(CircularDoublyLinkedList &&) = delete;
+class iterator
+{
+//----------------------------------------------------------------------------
+public:
+  iterator() = delete;
+  iterator(NodeT * node, std::size_t size);
+  iterator(NodeT * node, std::size_t pos, std::size_t size);
+  iterator(const iterator & rhs);
+//----------------------------------------------------------------------------
+  iterator & operator =(const iterator & rhs);
+//----------------------------------------------------------------------------
+  iterator & operator ++();
+  iterator & operator --();
+//----------------------------------------------------------------------------
+        ValueT & operator *();
+  const ValueT & operator *() const;
+//----------------------------------------------------------------------------
+        NodeT * operator ->();
+  const NodeT * operator ->() const;  
+//----------------------------------------------------------------------------
+  bool operator ==(const iterator & rhs) const;
+  bool operator !=(const iterator & rhs) const;
+//----------------------------------------------------------------------------
+private:
+  NodeT           * node_;
+  std::size_t       pos_;
+  bool              dir_;
+  const std::size_t size_;
+//----------------------------------------------------------------------------
+};  // class iterator
+//----------------------------------------------------------------------------
+  CircularDoublyLinkedList(const Alloc & alloc = Alloc());
   CircularDoublyLinkedList(std::initializer_list<ValueT> init, const Alloc & alloc = Alloc());
   ~CircularDoublyLinkedList();
 //----------------------------------------------------------------------------
-  ValueT & front();
+  std::size_t size() const;
+//----------------------------------------------------------------------------
+  bool empty() const;
+//----------------------------------------------------------------------------
+  void pushFront(const ValueT & val);
+  void pushFront(ValueT && val);
+//----------------------------------------------------------------------------
+  void popFront();
+//----------------------------------------------------------------------------
+  void pushBack(const ValueT & val);
+  void pushBack(ValueT && val);
+//----------------------------------------------------------------------------
+  void popBack();
+//----------------------------------------------------------------------------
+        ValueT & front();
   const ValueT & front() const;
 //----------------------------------------------------------------------------
-  ValueT & back();
+        ValueT & back();
   const ValueT & back() const;
+//----------------------------------------------------------------------------
+  iterator begin() const;
+  iterator end() const;
 //----------------------------------------------------------------------------
   void print() const;
   void printR() const;
 //----------------------------------------------------------------------------
 private:
-  Alloc         alloc_;
-  NodeT       * node_;
-  std::size_t   size_;
+  Alloc       alloc_;
+  NodeT     * node_;
+  std::size_t size_;
 //----------------------------------------------------------------------------
 }; // class CircularDoublyLinkedList
 //----------------------------------------------------------------------------
 template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator::iterator(NodeT * node, std::size_t size) :
+  node_(node),
+  pos_(0),
+  dir_(false),
+  size_(size)
+{
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator::iterator(NodeT * node, std::size_t pos, std::size_t size) :
+  node_(node),
+  pos_(pos),
+  dir_(false),
+  size_(size)
+{
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator::iterator(const iterator & rhs) :
+  node_(rhs.node_),
+  pos_(rhs.pos_),
+  dir_(rhs.dir_),
+  size_(rhs.size_)
+{
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator & CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator =(const iterator & rhs)
+{
+  if (*this != rhs)
+  {
+    node_ = rhs.node_;
+    pos_  = rhs.pos_;
+    dir_  = rhs.dir_;
+  }
+  return *this;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator & CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator++()
+{
+  node_ = node_->next_;
+  if (pos_ == 0)
+    dir_ = true; // true = forward
+  pos_ += (dir_ ? 1 : -1);
+  // NOTE: to handle overflow and underflow
+  pos_  = std::min(pos_, std::max(size_ - 1, (std::size_t)1));
+  return *this;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator & CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator--()
+{
+  node_ = node_->prev_;
+  if (pos_ == 0)
+    dir_ = false; // false = backward
+  pos_ += (dir_ ? -1 : 1);
+  // NOTE: to handle overflow and underflow
+  pos_ = std::min(pos_, std::max(size_ - 1, (std::size_t)1));
+  return *this;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+ValueT & CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator *()
+{
+  return node_->value_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+const ValueT & CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator *() const
+{
+  return node_->value_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::NodeT * CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator ->()
+{
+  return node_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+const CircularDoublyLinkedList<ValueT, Alloc>::NodeT * CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator ->() const
+{
+  return node_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+bool CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator ==(const iterator & rhs) const
+{
+  return node_ == rhs.node_ && 
+         pos_  == rhs.pos_  &&
+         size_ == rhs.size_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+bool CircularDoublyLinkedList<ValueT, Alloc>::iterator::operator !=(const iterator & rhs) const
+{
+  return !((*this) == rhs);
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::CircularDoublyLinkedList(const Alloc & alloc) :
+  alloc_(alloc),
+  node_(nullptr),
+  size_(0)
+{
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
 CircularDoublyLinkedList<ValueT, Alloc>::CircularDoublyLinkedList(std::initializer_list<ValueT> init, const Alloc & alloc) :
   alloc_(alloc),
+  node_(nullptr),
   size_(init.size())
 {
-  assert(size_ > 0);
   NodeT * first = nullptr;
   NodeT * last  = nullptr;
   auto it = init.end();
@@ -99,13 +252,126 @@ CircularDoublyLinkedList<ValueT, Alloc>::~CircularDoublyLinkedList()
 {
   NodeT * cur  = node_;
   NodeT * next = nullptr;
-  do
+  if (size_ != 0)
   {
-    cur->prev_->next_ = nullptr;
-    next = cur->next_;
-    alloc_.delete_object(cur);
-    cur = next;
-  } while (cur != nullptr);
+    do
+    {
+      cur->prev_->next_ = nullptr;
+      next = cur->next_;
+      alloc_.delete_object(cur);
+      cur = next;
+    } while (cur != nullptr);
+  }
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+std::size_t CircularDoublyLinkedList<ValueT, Alloc>::size() const
+{
+  return size_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+bool CircularDoublyLinkedList<ValueT, Alloc>::empty() const
+{
+  return size_ == 0;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::pushFront(const ValueT & val)
+{
+  ++size_;
+  if (node_ == nullptr)
+  {
+    node_ = alloc_.template new_object<NodeT>(val);
+    node_->next_ = node_;
+    node_->prev_ = node_;
+    return;
+  }
+  NodeT * prev = node_->prev_;
+  NodeT * next = node_;
+  node_ = alloc_.template new_object<NodeT>(val);
+  node_->prev_ = prev;
+  node_->next_ = next;
+  prev->next_ = node_;
+  next->prev_ = node_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::pushFront(ValueT && val)
+{
+  ++size_;
+  if (node_ == nullptr)
+  {
+    node_ = alloc_.template new_object<NodeT>(std::forward<ValueT>(val));
+    node_->next_ = node_;
+    node_->prev_ = node_;
+    return;
+  }
+  NodeT * prev = node_->prev_;
+  NodeT * next = node_;
+  node_ = alloc_.template new_object<NodeT>(std::forward<ValueT>(val));
+  node_->prev_ = prev;
+  node_->next_ = next;
+  prev->next_ = node_;
+  next->prev_ = node_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::popFront()
+{
+  --size_;
+  NodeT * prev = node_->prev_;
+  NodeT * next = node_->next_;
+  alloc_.delete_object(node_);
+  prev->next_ = next;
+  next->prev_ = prev;
+  node_ = next;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::pushBack(const ValueT & val)
+{
+  ++size_;
+  if (node_ == nullptr)
+  {
+    node_ = alloc_.template new_object<NodeT>(val);
+    node_->next_ = node_;
+    node_->prev_ = node_;
+    return;
+  }
+  NodeT * prev        = node_->prev_;
+  node_->prev_        = alloc_.template new_object<NodeT>(val);
+  node_->prev_->next_ = node_;
+  node_->prev_->prev_ = prev;
+  prev->next_         = node_->prev_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::pushBack(ValueT && val)
+{
+  ++size_;
+  if (node_ == nullptr)
+  {
+    node_        = alloc_.template new_object<NodeT>(val);
+    node_->next_ = node_;
+    node_->prev_ = node_;
+    return;
+  }
+  NodeT * prev        = node_->prev_;
+  node_->prev_        = alloc_.template new_object<NodeT>(std::forward<ValueT>(val));
+  node_->prev_->next_ = node_;
+  node_->prev_->prev_ = prev;
+  prev->next_         = node_->prev_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+void CircularDoublyLinkedList<ValueT, Alloc>::popBack()
+{
+  --size_;
+  NodeT * prev = node_->prev_->prev_;
+  alloc_.delete_object(node_->prev_);
+  node_->prev_ = prev;
+  prev->next_ = node_;
 }
 //----------------------------------------------------------------------------
 template<class ValueT, class Alloc>
@@ -130,6 +396,18 @@ template<class ValueT, class Alloc>
 const ValueT & CircularDoublyLinkedList<ValueT, Alloc>::back() const
 {
   return node_->prev_->value_;
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator CircularDoublyLinkedList<ValueT, Alloc>::begin() const
+{
+  return iterator(node_, size_);
+}
+//----------------------------------------------------------------------------
+template<class ValueT, class Alloc>
+CircularDoublyLinkedList<ValueT, Alloc>::iterator CircularDoublyLinkedList<ValueT, Alloc>::end() const
+{
+  return iterator(node_, std::max(size_-1, (std::size_t)1), size_);
 }
 //----------------------------------------------------------------------------
 template<class ValueT, class Alloc>
--- tests/circular_doubly_linked_list_tests.cpp
+++ tests/circular_doubly_linked_list_tests.cpp
@@ -6,19 +6,175 @@
 TEST(CircularDoublyLinkedListTest, Initialization)
 {
   {
+    CircularDoublyLinkedList<int> list;
+    EXPECT_EQ(0, list.size());
+    EXPECT_TRUE(list.empty());
+  }
+  {
     CircularDoublyLinkedList<int> list = {1};
+    ASSERT_EQ(1, list.size());
     EXPECT_EQ(1, list.back());
     EXPECT_EQ(1, list.front());
   }
   {
     CircularDoublyLinkedList<int> list = {1, 2};
+    ASSERT_EQ(2, list.size());
     EXPECT_EQ(1, list.front());
     EXPECT_EQ(2, list.back());
   }
   {
     CircularDoublyLinkedList<int> list = {1, 2, 3};
+    ASSERT_EQ(3, list.size());
     EXPECT_EQ(1, list.front());
     EXPECT_EQ(3, list.back());
+  }
+}
+//----------------------------------------------------------------------------
+TEST(CircularDoublyLinkedListTest, Iterators)
+{
+  {
+    CircularDoublyLinkedList<int> list = {1};
+    auto it = list.begin();
+    std::size_t times = 0;
+    for ( ; it != list.end(); ++it)
+      ++times;
+    EXPECT_EQ(1, times);
+  }
+  {
+    CircularDoublyLinkedList<int> list = {1, 2, 3};
+    auto it = list.begin();
+    auto end = list.end();
+    EXPECT_FALSE(it == end);
+    EXPECT_EQ(1, it->value_);
+    ++it;
+    --it;
+    EXPECT_FALSE(it == end);
+    ++it;
+    EXPECT_EQ(2, it->value_);
+    ++it;
+    EXPECT_EQ(3, it->value_);
+    ++it;
+    EXPECT_TRUE(it == end);
+    EXPECT_EQ(1, it->value_);
+  }
+}
+//----------------------------------------------------------------------------
+TEST(CircularDoublyLinkedListTest, PushBack)
+{
+  {
+    CircularDoublyLinkedList<int> list;
+    ASSERT_TRUE(list.empty());
+
+    list.pushBack(1);
+    ASSERT_EQ(1, list.size());
+    EXPECT_EQ(1, list.front());
+    EXPECT_EQ(1, list.back());
+
+    list.pushBack(2);
+    ASSERT_EQ(2, list.size());
+    EXPECT_EQ(1, list.front());
+    EXPECT_EQ(2, list.back());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    for (int i = 0; i < 1000; ++i)
+      list.pushBack(i);
+
+    EXPECT_EQ(1000, list.size());
+    EXPECT_EQ(0, list.front());
+    EXPECT_EQ(999, list.back());
+  }
+}
+//----------------------------------------------------------------------------
+TEST(CircularDoublyLinkedListTest, PopBack)
+{
+  {
+    CircularDoublyLinkedList<int> list;
+
+    list.pushBack(1);
+    list.popBack();
+    ASSERT_EQ(0, list.size());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+    list.popBack();
+    ASSERT_EQ(2, list.size());
+    EXPECT_EQ(1, list.front());
+    EXPECT_EQ(2, list.back());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    for (int i = 0; i < 1000; ++i)
+      list.pushBack(i);
+    while (list.empty() == false)
+      list.popBack();
+  }
+}
+//----------------------------------------------------------------------------
+TEST(CircularDoublyLinkedListTest, PushFront)
+{
+  {
+    CircularDoublyLinkedList<int> list;
+
+    list.pushFront(1);
+    ASSERT_EQ(1, list.size());
+    EXPECT_EQ(1, list.front());
+    list.pushFront(2);
+    ASSERT_EQ(2, list.size());
+    EXPECT_EQ(2, list.front());
+    list.pushFront(3);
+    ASSERT_EQ(3, list.size());
+    EXPECT_EQ(3, list.front());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    for (std::size_t i = 0; i < 1000; ++i)
+    {
+      list.pushFront(i);
+      EXPECT_EQ(i, list.front());
+    }
+  }
+}
+//----------------------------------------------------------------------------
+TEST(CircularDoublyLinkedListTest, PopFront)
+{
+  {
+    CircularDoublyLinkedList<int> list;
+
+    list.pushFront(1);
+    list.popFront();
+    ASSERT_EQ(0, list.size());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    list.pushFront(1);
+    list.pushFront(2);
+    list.pushFront(3);
+    ASSERT_EQ(3, list.size());
+    list.popFront();
+    ASSERT_EQ(2, list.size());
+    EXPECT_EQ(2, list.front());
+    EXPECT_EQ(1, list.back());
+    list.popFront();
+    ASSERT_EQ(1, list.size());
+    EXPECT_EQ(1, list.front());
+    EXPECT_EQ(1, list.back());
+  }
+  {
+    CircularDoublyLinkedList<int> list;
+
+    for (std::size_t i = 0; i < 10; ++i)
+      list.pushFront(i);
+    while (list.empty() == false)
+      list.popFront();
   }
 }
 //----------------------------------------------------------------------------
